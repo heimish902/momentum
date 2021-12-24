bb5915b674086c0b2f8bf3ebd4acdd9d89b0e3bd
heimish902 <heimish902@gmail.com>
2021-12-25 01:48:57 +0900

    2021-12-25

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -1,4 +1,4 @@
 const images = ['bg1.jpg', 'bg2.jpg', 'bg3.jpg'];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url('../image/${chosenImage}')`;
+document.body.style.backgroundImage = `url('./image/${chosenImage}')`;
--- momentum/js/music.js
+++ momentum/js/music.js
@@ -1,101 +1,109 @@
+'use strict';
+
+/* menu */
 const menu = document.querySelector('.menu');
+const bar = document.querySelectorAll('.bar');
+
 const player = document.querySelector('.player');
-const controls = document.querySelector('.controls');
-const span = controls.querySelectorAll('span');
+
+const cd = document.querySelector('.big');
 const title = document.querySelector('.music_title');
 const desc = document.querySelector('.music_desc');
 
-const cd = document.querySelector('.big');
+/* controls */
+const controls = document.querySelector('.controls');
 const audio = document.querySelector('audio');
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
+const play = controls.querySelector('.play');
 
+let index = 0;
 let playlist = [
-  {
-    title: 'Blizzards',
-    music: 'Blizzards.mp3',
-  },
-  {
-    title: 'Calm',
-    music: 'Calm.mp3',
-  },
-  {
-    title: 'Dusty_Road',
-    music: 'Dusty_Road.mp3',
-  },
-  {
-    title: 'Escape',
-    music: 'Escape.mp3',
-  },
-  {
-    title: 'Payday',
-    music: 'Payday.mp3',
-  },
-  {
-    title: 'Retreat',
-    music: 'Retreat.mp3',
-  },
-  {
-    title: 'Seasonal',
-    music: 'Seasonal.mp3',
-  },
-  {
-    title: 'Vespers',
-    music: 'Vespers.mp3',
-  },
+  'Blizzards',
+  'Calm',
+  'Dusty_Road',
+  'Escape',
+  'Payday',
+  'Retreat',
+  'Seasonal',
+  'Vespers',
 ];
 
-function showMusic() {
-  player.style.display = 'block';
+// change menu icon
+function changeMenuIcon() {
+  bar.forEach((el) => {
+    el.classList.toggle('change');
+  });
+  playerControl();
 }
 
-function closeMusic() {
-  player.style.display = 'none';
+// show , hide player
+function showPlayer() {
+  player.classList.add('on');
 }
 
-function controlMusic(e) {
-  if (e.target == span[0]) {
-    cd.classList.remove('play');
-    audio.pause();
-  }
-  if (e.target == span[1]) {
-    cd.classList.add('play');
-    audio.play();
-  }
-  if (e.target == span[2]) {
-    cd.classList.remove('play');
-    audio.pause();
-    audio.currentTime = 0;
+function hidePlayer() {
+  player.classList.remove('on');
+}
+
+function playerControl() {
+  const isOn = player.classList.contains('on');
+  if (!isOn) {
+    showPlayer();
+  } else {
+    hidePlayer();
   }
 }
 
-let num = 0;
+// prev, next music
 function prevMusic() {
-  num -= 1;
-  let index = playlist.length + num;
-  audio.src = `../audio/${playlist[index].music}`;
-  audio.play();
-
-  title.innerText = `${playlist[index].title}`;
-
-  if (num == -playlist.length) {
-    num = 0;
+  index--;
+  if (index < 0) {
+    index = playlist.length - 1;
   }
+  changeMusic(playlist[index]);
 }
 
 function nextMusic() {
-  num++;
-  audio.src = `../audio/${playlist[num].music}`;
+  index++;
+  if (index > playlist.length - 1) {
+    index = 0;
+  }
+  changeMusic(playlist[index]);
+}
+
+function changeMusic(num) {
+  audio.src = `audio/${num}.mp3`;
+  title.innerText = `${num}`;
+  playMusic();
+}
+
+// play , pause
+function playMusic() {
+  cd.classList.add('play');
+  play.innerText = 'pause';
+  play.className = play.className.replace('play', 'pause');
+
   audio.play();
+  audio.muted = false;
+}
+function pauseMusic() {
+  cd.classList.remove('play');
+  play.innerText = 'play_arrow';
+  play.className = play.className.replace('pause', 'play');
 
-  title.innerText = `${playlist[num].title}`;
-
-  if (num == playlist.length - 1) {
-    num = -1;
+  audio.pause();
+}
+function playToPause() {
+  const isPlaying = play.classList.contains('play');
+  if (isPlaying) {
+    playMusic();
+  } else {
+    pauseMusic();
   }
 }
 
-menu.addEventListener('click', showMusic);
-controls.addEventListener('click', controlMusic);
+menu.addEventListener('click', changeMenuIcon);
+play.addEventListener('click', playToPause);
 prev.addEventListener('click', prevMusic);
 next.addEventListener('click', nextMusic);
